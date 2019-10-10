8fd8dc2b87f5c1c585cf73a6832670de50770086
Luke Howard <binaryluke@gmail.com>
2019-10-10 22:25:30 +1100

    Draw a line through Clayton and centre map on it

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import React from 'react';
 import DeckGL from '@deck.gl/react';
 import {LineLayer} from '@deck.gl/layers';
 import {StaticMap} from 'react-map-gl';
+import dataset from './data/clayton.response.json';
 
 import './App.css';
+
+window.dataset = dataset;
 
 // Set your mapbox access token here
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYmluYXJ5bHVrZSIsImEiOiJjazFqNWIyNTYxM3d2M2dzMGc5eHJmdDV6In0.GSRpcHwrbqB3IWKrwaDKUg';
 
 // Data to be used by the LineLayer
-const data = [{sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}];
+const data = [{sourcePosition: [145.0603463, -37.903704], targetPosition: [145.1824093, -37.956786]}];
 
 // Viewport settings
 const initialViewState = {
-  longitude: -122.41669,
-  latitude: 37.7853,
+  longitude: 145.123716,
+  latitude: -37.9310053,
   zoom: 13,
   pitch: 0,
   bearing: 0
