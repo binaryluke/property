84462dd510acf2e9bc7ca8d6f05c895635f4f125
Luke Howard <binaryluke@gmail.com>
2019-10-10 21:53:02 +1100

    Use MapBox to render line onto Map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import DeckGL from '@deck.gl/react';
+import {LineLayer} from '@deck.gl/layers';
+import {StaticMap} from 'react-map-gl';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="container">
+// Set your mapbox access token here
+const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYmluYXJ5bHVrZSIsImEiOiJjazFqNWIyNTYxM3d2M2dzMGc5eHJmdDV6In0.GSRpcHwrbqB3IWKrwaDKUg';
 
-    </div>
+// Data to be used by the LineLayer
+const data = [{sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}];
+
+// Viewport settings
+const initialViewState = {
+  longitude: -122.41669,
+  latitude: 37.7853,
+  zoom: 13,
+  pitch: 0,
+  bearing: 0
+};
+
+function App() {
+  const layers = [
+    new LineLayer({id: 'line-layer', data})
+  ];
+
+  return (
+    <DeckGL
+      initialViewState={initialViewState}
+      controller={true}
+      layers={layers}
+    >
+      <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
+    </DeckGL>
   );
 }
 
