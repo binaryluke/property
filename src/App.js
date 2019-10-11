e79afcd2ff37adfbf83bb0b35ff0eddb65bab187
Luke Howard <binaryluke@gmail.com>
2019-10-11 21:49:31 +1100

    Switch to using a GridCellLayer + color code property type + style map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import DeckGL from '@deck.gl/react';
-import {ScatterplotLayer} from '@deck.gl/layers';
+import {GridCellLayer} from '@deck.gl/layers';
 import {StaticMap} from 'react-map-gl';
 import dataset from './data/clayton.response.json';
 
 import './App.css';
+
+const PROPERTY_TYPE_HOUSE = 'House';
+const PROPERTY_TYPE_TOWNHOUSE = 'Townhouse';
+const PROPERTY_TYPE_AUF = 'ApartmentUnitFlat';
 
 // Set your mapbox access token here
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYmluYXJ5bHVrZSIsImEiOiJjazFqNWIyNTYxM3d2M2dzMGc5eHJmdDV6In0.GSRpcHwrbqB3IWKrwaDKUg';
@@ -12,8 +16,14 @@ const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYmluYXJ5bHVrZSIsImEiOiJjazFqNWIyNTYxM3d2
 const listings = dataset
   .filter(item => item.type === 'PropertyListing')
   .map(item => item.listing)
+  .filter(listing => [
+    PROPERTY_TYPE_HOUSE,
+    PROPERTY_TYPE_TOWNHOUSE,
+    PROPERTY_TYPE_AUF
+  ].includes(listing.propertyDetails.propertyType))
   .map(listing => ({
     id: listing.id,
+    type: listing.propertyDetails.propertyType,
     price: listing.priceDetails.price,
     coordinates: [
       listing.propertyDetails.longitude,
@@ -22,14 +32,26 @@ const listings = dataset
   }));
 
 const layer = {
-  id: 'listings',
+  id: 'grid-cell-layer',
   data: listings,
-  stroked: false,
-  filled: true,
+  pickable: true,
+  extruded: true,
+  cellSize: 50,
+  elevationScale: 1,
   getPosition: d => d.coordinates,
-  getRadius: d => Math.sqrt(d.price)/8,
-  getFillColor: [255, 200, 0]
-};
+  getFillColor: d => {
+    if (d.type === PROPERTY_TYPE_HOUSE) return [255, 65, 51]; // #ff4133
+    else if (d.type === PROPERTY_TYPE_TOWNHOUSE) return [51, 139, 255]; // #338bff
+    else if (d.type === PROPERTY_TYPE_AUF) return [51, 255, 167]; // #33ffa7
+    return [0, 0, 0];
+  },
+  getElevation: d => Math.sqrt(d.price),//d.value,
+  onHover: ({object, x, y}) => {
+    if (!object) return;
+    const tooltip = `price: ${object.price}`;
+    console.log(tooltip);
+  }
+}
 
 
 // Viewport settings
@@ -43,7 +65,7 @@ const initialViewState = {
 
 function App() {
   const layers = [
-    new ScatterplotLayer(layer)
+    new GridCellLayer(layer)
   ];
 
   return (
@@ -52,7 +74,10 @@ function App() {
       controller={true}
       layers={layers}
     >
-      <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
+      <StaticMap
+        mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
+        mapStyle="mapbox://styles/mapbox/dark-v9"
+      />
     </DeckGL>
   );
 }
