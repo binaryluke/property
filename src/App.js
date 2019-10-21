5e56a1e24e400667f2e3358fad4d70dd780c5573
Luke Howard <binaryluke@gmail.com>
2019-10-22 08:02:49 +1100

    Finish the header/title area

--- src/App.js
+++ src/App.js
@@ -38,58 +38,60 @@ function App() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.title}>
+      <div className={styles.titleContainer}>
         <Title />
       </div>
-      <div className={styles.map}>
-        <Map
-          token={mapToken}
-          listings={listings}
-          onChangeSelectedListing={setSelectedListingId}
-          onMapMove={(center=[], nw=[], se=[]) => {
-            // Handle edge case where map sends us undefined prior to map initilisation
-            if (center.length !== 2 || nw.length !== 2 || se.length !== 2) return;
+      <div className={styles.contentContainer}>
+        <div className={styles.map}>
+          <Map
+            token={mapToken}
+            listings={listings}
+            onChangeSelectedListing={setSelectedListingId}
+            onMapMove={(center=[], nw=[], se=[]) => {
+              // Handle edge case where map sends us undefined prior to map initilisation
+              if (center.length !== 2 || nw.length !== 2 || se.length !== 2) return;
 
-            // Parameters sent in format `[longitude, latitude]`
-            setMapBounds({
-              centerLon: center[0],
-              centerLat: center[1],
-              nwLon: nw[0],
-              nwLat: nw[1],
-              seLon: se[0],
-              seLat: se[1],
-            });
-          }}
-        />
-      </div>
-      <div className={styles.listing}>
-        {selectedListing && <Listing
-          displayPrice={selectedListing && selectedListing.displayPrice}
-          url={selectedListing && selectedListing.url}
-          images={selectedListing && selectedListing.images}
-        />}
-        {!selectedListing && <div className={styles.noListing}>
-          <div>Click on a listing to see details</div>
-        </div>}
-      </div>
-      <div className={styles.status}>
-        <Status
-          numListings={listings.length}
-          isListingLimitExceeded={isListingLimitExceeded}
-          isSearchDisabled={lastSearchMapBounds === mapBounds}
-          onClickSearch={() => {
-            setLastSearchMapBounds(mapBounds);
-            getListings(mapBounds)
-              .then(({data}) => {
-                const {listings, isListingLimitExceeded} = data;
-                setListings(listings);
-                setIsListingLimitExceeded(isListingLimitExceeded);
+              // Parameters sent in format `[longitude, latitude]`
+              setMapBounds({
+                centerLon: center[0],
+                centerLat: center[1],
+                nwLon: nw[0],
+                nwLat: nw[1],
+                seLon: se[0],
+                seLat: se[1],
               });
-          }}
-        />
-      </div>
-      <div className={styles.legend}>
-        <Legend />
+            }}
+          />
+        </div>
+        <div className={styles.listing}>
+          {selectedListing && <Listing
+            displayPrice={selectedListing && selectedListing.displayPrice}
+            url={selectedListing && selectedListing.url}
+            images={selectedListing && selectedListing.images}
+          />}
+          {!selectedListing && <div className={styles.noListing}>
+            <div>Click on a listing to see details</div>
+          </div>}
+        </div>
+        <div className={styles.status}>
+          <Status
+            numListings={listings.length}
+            isListingLimitExceeded={isListingLimitExceeded}
+            isSearchDisabled={lastSearchMapBounds === mapBounds}
+            onClickSearch={() => {
+              setLastSearchMapBounds(mapBounds);
+              getListings(mapBounds)
+                .then(({data}) => {
+                  const {listings, isListingLimitExceeded} = data;
+                  setListings(listings);
+                  setIsListingLimitExceeded(isListingLimitExceeded);
+                });
+            }}
+          />
+        </div>
+        <div className={styles.legend}>
+          <Legend />
+        </div>
       </div>
     </div>
   );
